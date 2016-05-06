476efcc2c56b67bcfa5e212649ef2bfa524722e5
SebastienGrana <granarolo.sebastien@gmail.com>
2016-05-06 13:23:45 +0200

    FINI peutetre

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -3,11 +3,20 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 		int number=1;
-		int lastnumber = 0;
+		int number2=1;
+		int interNumber=0;
+		System.out.println(" " + 0);
+		System.out.println(" " + number);
+		System.out.println(" " + number2);
+		
 		for (int i = 1; i < 20; i++) {
-			lastnumber = number;
-			System.out.println(" " + number);
-			number = lastnumber + number;
+			
+			
+			interNumber = number2 + number; //calcule n3 = n1 + n2
+			System.out.println(" " + interNumber);
+			number2 = number;
+			number = interNumber;
+			
 			
 			}
 		}
